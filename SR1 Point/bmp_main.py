dd41b7e58aad249a1862be8643f6bd21d162c564
unclepete-20 <pedroparty310@gmail.com>
2022-08-06 18:21:43 -0600

    Se han reorganizado los archivos y se ha implementado la funcion de lineas

--- SR1 Point/bmp_main.py
+++ SR1 Point/bmp_main.py
@@ -2,9 +2,9 @@ from bmp_renderer import Render
 
 frame = Render()
 
-frame.glCreateWindow(20, 20)
+frame.glCreateWindow(100, 100)
 
-frame.glViewPort(2, 2, 1, 1)
+frame.glViewPort(20, 20, 10, 10)
 
 frame.glClearColor(0.25, 1, 0.50)
 
